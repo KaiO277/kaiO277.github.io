301659d31545f3a4fe1972c63b93c0e1f1f674bb
KaiO277 <nghiatran1527@gmail.com>
2023-06-05 16:38:35 +0700

    KaiO

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+
+  
+

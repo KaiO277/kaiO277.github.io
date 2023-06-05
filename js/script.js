9a183b864dc5e6c5787001e652e1090eb7f29bec
KaiO277 <nghiatran1527@gmail.com>
2023-06-05 16:47:57 +0700

    KaiO

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
 
 var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
